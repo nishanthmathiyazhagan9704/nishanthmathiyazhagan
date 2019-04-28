1db98b3d8606a606083406a5d2a74323f2b44e24
nishanth <nishanthmathi628@gmail.com>
2019-04-28 23:38:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="shortcut icon" type="image/png" href="images/favicon.png">
+  <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+
+  <title>party scenes</title>
+</head>
+  <style media="screen">
+
+
+    .rg{
+      background: violet;
+    }
+
+    .btn{
+        background:  violet;
+        width: 200px;
+        height: 50px;
+        border-radius: 0;
+    }
+  </style>
+<body>
+
+  <nav class="navbar navbar-inverse navbar-toggleable-sm">
+
+    <button class="navbar-toggler navbar-dark navbar-toggler-right" type="button" data-toggle="collapse" data-target="#myContent" aria-controls="myContent" aria-lable="Toggle-Navigation"
+    aira-expanded="false">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <a href="index.html" class="navbar-brand">party scenes</a>
+
+    <div class="collapse navbar-collapse" id="myContent">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="index.html">Home</a>
+        <a class="nav-item nav-link active" href="aboutus.html">About Us</a>
+        <a class="nav-item nav-link active" href="events.html">Events</a>
+        <a class="nav-item nav-link active" href="register.html">Register</a>
+        <a class="nav-item nav-link active" href="contactus.html">Contact Us</a>
+      </div><!-- navbar-nav -->
+    </div>
+
+  </nav><!-- nav -->
+  <header class="jumbotron jumbotron-fluid text-warning mb-0 bg header">
+      <div class="container main-head">
+      <div class="display-4 mb-4">party scenes</div>
+      <p class="lead" style="background:rgba(0,0,0,0.1);padding:30px;border:0.2px solid rgba(255,255,255,0.2);">party scenes is where you can bring yourself delight.<span class="mq"> We are fueled towards party scenes. Catch us on 23th and 24th of March 2018.</span><strong> SO LETS party TOGETHER </strong></p>
+      <button class="btn btn-lg" type="button" name="button"><a class="text-white" href="events.html">Register</a></button>
+      </div>
+  </header>
+
+
+
+
+<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+<script src="js/jquery.slim.min.js"></script>
+<script src="js/tether.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/scripts.js"></script>
+</body>
+</html>
